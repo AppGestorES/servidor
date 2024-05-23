52ba16bb6d98fda8e0dd9d874a29f5493301509e
Zas <thezasm03@gmail.com>
2024-05-23 18:28:23 +0200

    Fix nombres

--- src/routes/v1/salidaProductos/index.ts
+++ src/routes/v1/salidaProductos/index.ts
@@ -1,16 +1,16 @@
 import express from "express";
 
-import { Salidas } from "@controllers/salidaProductos/salidaProductos";
+import { SalidaProductos } from "@controllers/salidaProductos/salidaProductos";
 
 const router = express.Router();
 
 // rutas
-router.get("/salidas", new Salidas().getSalidas);
-router.get("/salidas/producto_final/:producto_final_id", new Salidas().getSalidasByProductoFinal);
-router.get("/salidas/fecha_salida/:fecha_salida", new Salidas().getSalidasByFechaSalida);
-router.get("/salidas/proyecto/:id_proyecto", new Salidas().getSalidasByProyecto);
-router.post("/salidas", new Salidas().postSalidas);
-router.put("/salidas/:id", new Salidas().putSalidas);
-router.delete("/salidas/:id", new Salidas().deleteSalidas);
+router.get("/salidas", new SalidaProductos().getSalidas);
+router.get("/salidas/producto_final/:producto_final_id", new SalidaProductos().getSalidasByProductoFinal);
+router.get("/salidas/fecha_salida/:fecha_salida", new SalidaProductos().getSalidasByFechaSalida);
+router.get("/salidas/proyecto/:id_proyecto", new SalidaProductos().getSalidasByProyecto);
+router.post("/salidas", new SalidaProductos().postSalidas);
+router.put("/salidas/:id", new SalidaProductos().putSalidas);
+router.delete("/salidas/:id", new SalidaProductos().deleteSalidas);
 
 export default router;
