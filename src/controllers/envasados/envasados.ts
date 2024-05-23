c3fcf7bc1578f8cae8bf02f053d9c32602bdb362
Zas <thezasm03@gmail.com>
2024-05-23 16:18:09 +0200

    Envasados hechos

---
+++ src/controllers/envasados/envasados.ts
@@ -0,0 +1,132 @@
+import { Request, Response, NextFunction } from "express";
+import pool from "@config/db";
+
+import tryCatch from "@utils/tryCatch";
+import { STATUS_OK, resultHandler } from "@middlewares/resultHandler";
+
+import {
+    getEnvasadosService,
+    getEnvasadosByNombreService,
+    getEnvasadosByProyectoService,
+    postEnvasadosService,
+    putEnvasadosService,
+    deleteEnvasadosService,
+} from "@services/envasadosService";
+import {
+    postEnvasadosInterface,
+    putEnvasadosInterface,
+} from "@interfaces/envasados.interface";
+
+export class Envasados {
+    async getEnvasados(req: Request, res: Response, next: NextFunction) {
+        await tryCatch(
+            async (req: Request, res: Response, next: NextFunction) => {
+                const conn = await pool.getConnection();
+                const results = await conn.query(getEnvasadosService);
+                resultHandler(
+                    { status: STATUS_OK, success: true, result: results },
+                    res,
+                    conn
+                );
+            }
+        )(req, res, next);
+    }
+
+    async getEnvasadosByNombre(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) {
+        await tryCatch(
+            async (req: Request, res: Response, next: NextFunction) => {
+                const conn = await pool.getConnection();
+                const nombre = `%${req.params.nombre}%`;
+                const results = await conn.query(getEnvasadosByNombreService, [
+                    nombre,
+                ]);
+                resultHandler(
+                    { status: STATUS_OK, success: true, result: results },
+                    res,
+                    conn
+                );
+            }
+        )(req, res, next);
+    }
+
+    async getEnvasadosByProyecto(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) {
+        await tryCatch(
+            async (req: Request, res: Response, next: NextFunction) => {
+                const conn = await pool.getConnection();
+                const id_proyecto = req.params.id_proyecto;
+                const results = await conn.query(
+                    getEnvasadosByProyectoService,
+                    [id_proyecto]
+                );
+                resultHandler(
+                    { status: STATUS_OK, success: true, result: results },
+                    res,
+                    conn
+                );
+            }
+        )(req, res, next);
+    }
+
+    async postEnvasados(req: Request, res: Response, next: NextFunction) {
+        await tryCatch(
+            async (req: Request, res: Response, next: NextFunction) => {
+                const conn = await pool.getConnection();
+                const { nombre, id_proyecto } =
+                    req.body as postEnvasadosInterface;
+                const results = await conn.query(postEnvasadosService, [
+                    nombre,
+                    id_proyecto,
+                ]);
+                resultHandler(
+                    { status: STATUS_OK, success: true, result: results },
+                    res,
+                    conn
+                );
+            }
+        )(req, res, next);
+    }
+
+    async putEnvasados(req: Request, res: Response, next: NextFunction) {
+        await tryCatch(
+            async (req: Request, res: Response, next: NextFunction) => {
+                const conn = await pool.getConnection();
+                const { nombre, id_proyecto } =
+                    req.body as putEnvasadosInterface;
+                const { id } = req.params;
+                const results = await conn.query(putEnvasadosService, [
+                    nombre,
+                    id_proyecto,
+                    id,
+                ]);
+                resultHandler(
+                    { status: STATUS_OK, success: true, result: results },
+                    res,
+                    conn
+                );
+            }
+        )(req, res, next);
+    }
+
+    async deleteEnvasados(req: Request, res: Response, next: NextFunction) {
+        await tryCatch(
+            async (req: Request, res: Response, next: NextFunction) => {
+                const conn = await pool.getConnection();
+                const { id } = req.params;
+                const results = await conn.query(deleteEnvasadosService, [id]);
+                resultHandler(
+                    { status: STATUS_OK, success: true, result: results },
+                    res,
+                    conn
+                );
+            }
+        )(req, res, next);
+    }
+}
--- src/services/envasadosService.ts
+++ src/services/envasadosService.ts
@@ -1,8 +1,8 @@
-const getEnvasadoService: string = "SELECT id, nombre, id_proyecto FROM envasado";
-const getEnvasadoByNombreService: string = "SELECT id, nombre, id_proyecto FROM envasado WHERE nombre LIKE ?";
-const getEnvasadoByProyectoService: string = "SELECT id, nombre, id_proyecto FROM envasado WHERE id_proyecto = ?";
-const postEnvasadoService: string = "INSERT INTO envasado (nombre, id_proyecto) VALUES (?, ?)";
-const putEnvasadoService: string = "UPDATE envasado SET nombre = ?, id_proyecto = ? WHERE id = ?";
-const deleteEnvasadoService: string = "DELETE FROM envasado WHERE id = ?";
+const getEnvasadosService: string = "SELECT id, nombre, id_proyecto FROM envasados";
+const getEnvasadosByNombreService: string = "SELECT id, nombre, id_proyecto FROM envasados WHERE nombre LIKE ?";
+const getEnvasadosByProyectoService: string = "SELECT id, nombre, id_proyecto FROM envasados WHERE id_proyecto = ?";
+const postEnvasadosService: string = "INSERT INTO envasados (nombre, id_proyecto) VALUES (?, ?)";
+const putEnvasadosService: string = "UPDATE envasados SET nombre = ?, id_proyecto = ? WHERE id = ?";
+const deleteEnvasadosService: string = "DELETE FROM envasados WHERE id = ?";
 
-export { getEnvasadoService, getEnvasadoByNombreService, getEnvasadoByProyectoService, postEnvasadoService, putEnvasadoService, deleteEnvasadoService };
+export { getEnvasadosService, getEnvasadosByNombreService, getEnvasadosByProyectoService, postEnvasadosService, putEnvasadosService, deleteEnvasadosService };
