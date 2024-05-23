1d41a983eb59fbafce0d6e89cfd5d6c3606ddf5e
Zas <thezasm03@gmail.com>
2024-05-23 17:01:50 +0200

    Endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import proyectos from "@routes/v1/proyectos";
 import pages from "@routes/v1/pages";
 import usuarios from "@routes/v1/usuarios";
 import sesiones from "@routes/v1/sesiones";
+import formatos from "@routes/v1/formatos"
 
 const router = Router();
 
@@ -12,6 +13,7 @@ router.use(materiasPrimas);
 router.use(proyectos);
 router.use(usuarios);
 router.use(sesiones);
+router.use(formatos);
 router.use(pages);
 
 export default router;
