280a60486f4bd8d3b683b3903953e0c23f9b096a
Zas <thezasm03@gmail.com>
2024-06-05 14:51:11 +0200

    Console.log quitado

--- src/utils/tryCatch.ts
+++ src/utils/tryCatch.ts
@@ -13,7 +13,6 @@ const tryCatch =
                 case "ER_ROW_IS_REFERENCED_2":
                     return next(new PrimaryKeyEntryError("La clave principal está siendo usada."));
                 case "ER_NO_REFERENCED_ROW_2":
-                    console.log(error)
                     return next(new ForeignKeyEntryError("La clave foránea no existe."));
             }
             console.log(error);
