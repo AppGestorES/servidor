d54ee0836b57e6a04e480f3dc035a6fbadc25298
Zas <thezasm03@gmail.com>
2024-06-05 17:05:43 +0200

    Proveedores hechos

--- src/routes/v1/proveedores/index.ts
+++ src/routes/v1/proveedores/index.ts
@@ -6,8 +6,14 @@ const router = express.Router();
 
 // rutas
 router.get("/proveedores", new Proveedores().getProveedores);
-router.get("/proveedores/nombre/:nombre", new Proveedores().getProveedoresByNombre);
-router.get("/proveedores/proyecto/:id_proyecto", new Proveedores().getProveedoresByProyecto);
+router.get(
+    "/proveedores/nombre/:nombre",
+    new Proveedores().getProveedoresByNombre
+);
+router.get(
+    "/proveedores/proyecto/:id_proyecto",
+    new Proveedores().getProveedoresByProyecto
+);
 router.post("/proveedores", new Proveedores().postProveedores);
 router.put("/proveedores/:id", new Proveedores().putProveedores);
 router.delete("/proveedores/:id", new Proveedores().deleteProveedores);
