9645a1eff6fef1a45af2dde9251ec0aafe2dca8b
Zas <thezasm03@gmail.com>
2024-06-09 23:28:47 +0200

    Delete arreglado

--- src/services/sesionesService.ts
+++ src/services/sesionesService.ts
@@ -1,4 +1,5 @@
 const postSesionesService: string = "INSERT INTO sesiones (token, fecha, usuario) VALUES (?, ?, ?)";
 const iniciarSesionService: string = "SELECT id, identificador, contrasena FROM usuarios WHERE identificador = ?"
+const deleteSesionService: string = "DELETE FROM sesiones WHERE usuario = ?"
 
-export { postSesionesService, iniciarSesionService };
+export { postSesionesService, iniciarSesionService, deleteSesionService };
